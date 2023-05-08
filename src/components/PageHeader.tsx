5cbd6eefb201531dd3cc5b6a5e5f5c80f130ec16
zshafe <zshafe@cs.washington.edu>
2023-05-08 12:04:10 -0700

    finished MUI refactor

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,30 +1,32 @@
 import React from "react";
 import Image from "next/image";
 import utilStyles from "fishwrapper/styles/utils.module.css";
-import { Link, Stack, Typography } from "@mui/material";
+import { Link, Paper, Stack, Typography } from "@mui/material";
 import { siteTitle } from "fishwrapper/components/layout";
 
 export default function PageHeader() {
   return (
-    <Stack
-      direction={"column"}
-      alignItems={"center"}
-      spacing={2}
-      sx={{ mb: 2 }}
-    >
-      <Link href="/">
-        <Image
-          priority
-          src="/images/fishwrapper.jpg"
-          className={utilStyles.borderCircle}
-          height={108}
-          width={108}
-          alt={siteTitle}
-        />
-      </Link>
-      <Link href="/" className={utilStyles.linkFixer}>
-        <Typography variant="h5">{siteTitle}</Typography>
-      </Link>
-    </Stack>
+    <Paper elevation={3} sx={{ backgroundColor: "#f9f7f1" }}>
+      <Stack
+        direction={"column"}
+        alignItems={"center"}
+        spacing={2}
+        sx={{ my: 2, py: 2 }}
+      >
+        <Link href="/">
+          <Image
+            priority
+            src="/images/fishwrapper.jpg"
+            className={utilStyles.borderCircle}
+            height={108}
+            width={108}
+            alt={siteTitle}
+          />
+        </Link>
+        <Link href="/" className={utilStyles.linkFixer}>
+          <Typography variant="h5">{siteTitle}</Typography>
+        </Link>
+      </Stack>
+    </Paper>
   );
 }

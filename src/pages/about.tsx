df7a8c6cc07de5c87f46446ba6d3ba89e2624d20
zshafe <zshafe@cs.washington.edu>
2023-05-10 12:37:46 -0700

    Updated Inquiries and About

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,10 @@
 import Layout from "fishwrapper/components/layout";
 import Head from "next/head";
 import { Paper, Typography } from "@mui/material";
+import {
+  Timeline,
+} from "@mui/lab";
+import AboutItem from "fishwrapper/components/AboutItem";
 
 export default function About() {
   return (
@@ -9,7 +13,23 @@ export default function About() {
         <title>About</title>
       </Head>
       <Paper elevation={3} sx={{ padding: 2, backgroundColor: "#f9f7f1" }}>
-        <Typography variant="h1">TODO: FINISH ABOUT PAGE</Typography>
+      {/* TODO: Write real fake timeline */}
+      <Typography variant="h4" align="center">
+          {"A Brief History"}
+        </Typography>
+        <Timeline position="alternate">
+          <AboutItem
+            title={"IT WAS A NEW BEGINNING"}
+            date={"2023-04-13"}
+            text={"It was time to be funny again"}
+          />
+          <AboutItem
+            title={"THEN THEY HUNG OUT AGAIN"}
+            date={"2023-04-25"}
+            text={"And made some progress"}
+            finalItem
+          />
+        </Timeline>
       </Paper>
     </Layout>
   );
